75018d8d1f0a790e5fc3002d029db702c3eca47d
Shadab khan <srjglobal.shadab.javadeveloper@gmail.com>
2025-09-06 18:58:22 +0530

    changes in backend 2

--- src/main/java/Ayurvedh/ayurvedh/Controllers/ProductController.java
+++ src/main/java/Ayurvedh/ayurvedh/Controllers/ProductController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,8 @@ import Ayurvedh.ayurvedh.entity.Products;
 import Ayurvedh.ayurvedh.entity.Category;
 import Ayurvedh.ayurvedh.entity.SubCategory;
 
-@RestController("/public")
+@RestController
+@RequestMapping("/public")
 @CrossOrigin(origins = "http://localhost:5173")
 
 public class ProductController {
